9c363591ec98c82623b44f1fb38aa315fb53895f
NicoPaez <nicopaez@gmail.com>
2013-05-11 11:41:23 -0300

    Bootstrap support added and create app layout

---
+++ app/controllers/events.rb
@@ -0,0 +1,24 @@
+Camilo::App.controllers :events do
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  get '/' do
+   @message = 'Under construction. Come back in a while'
+   render 'events/index' 
+  end
+  
+
+end
--- features/coming_soon.feature
+++ features/coming_soon.feature
@@ -5,4 +5,4 @@ Feature: Coming soon
 
   Scenario: Browse default
     When I go to "the home page"
-    Then I should see "Coming soon"
+    Then I should see "Gracias por tu visita"
