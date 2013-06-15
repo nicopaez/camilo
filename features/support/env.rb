2c48a4989452acbb39f58e1b4e4f8843019b1a2c
NicoPaez <nicopaez@gmail.com>
2013-06-15 10:09:38 -0300

    Simplecov configured for cucumber tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../config/boot")
 
+
 require 'capybara/cucumber'
 require 'rspec/expectations'
+require 'simplecov'
+
+SimpleCov.start do
+  root(File.join(File.dirname(__FILE__), '../../'))
+  add_filter '/spec/'
+  add_filter '/features/'
+end
 
 ##
 # You can handle all padrino applications using instead:
